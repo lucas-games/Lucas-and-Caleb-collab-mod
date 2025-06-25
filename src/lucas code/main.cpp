3899e5781aef056ff78ad76a8700f707096ab444
Lucas Fry <lucas.e.fry@outlook.com>
2025-06-25 10:28:15 -0700

    Fix syntax error by adding a missing semicolon in the playEffect

--- src/lucas code/main.cpp
+++ src/lucas code/main.cpp
@@ -24,7 +24,7 @@ class $modify(MyMenuLayer, MenuLayer) {
 
 		menu->updateLayout();
 
-		FMODAudioEngine::sharedEngine()->playEffect("geometry-dash.mp3"_spr)
+		FMODAudioEngine::sharedEngine()->playEffect("geometry-dash.mp3"_spr);
 
 		return true;
 	}
